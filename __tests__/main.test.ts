fe9451a63228aab2427e3c9c79ca0c2a374b4aa2
James Creese <james.creese@iress.com>
2021-10-20 19:22:39 +0100

    typescript

---
+++ __tests__/main.test.ts
@@ -0,0 +1,5 @@
+import {expect, test} from '@jest/globals'
+
+test('random test', () => {
+    expect(true).toBeTruthy();
+});
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+import {getInput, setFailed, setOutput} from '@actions/core'
+import {context} from '@actions/github'
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = getInput('who-to-greet')
+  console.log(`Hello ${nameToGreet}!`)
+
+  const time = new Date().toTimeString()
+  setOutput('time', time)
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`)
+} catch (error: unknown) {
+  setFailed((error as Error).message)
+}
